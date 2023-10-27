6a201c34ec7f5998846356e039d717610124fdbf
memak <klevis.mema@ulb.be>
2023-10-27 16:48:31 +0200

    - Finished with all the comments with login and register logic - Minor fixes to data types in model - Added and fixed some loggings - Added user managment - Added a internal server error logger helper

---
+++ SafeShare.Utilities/Log/Util_LogsHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SafeShare.Utilities.IP;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using SafeShare.Utilities.Responses;
+
+namespace SafeShare.Utilities.Log;
+
+public static class Util_LogsHelper<TReturnType, TLogger>
+{
+
+    public static async Task<Util_GenericResponse<TReturnType>>
+    ReturnInternalServerError
+    (
+        Exception ex,
+        ILogger<TLogger> _logger,
+        string errorMessage,
+        TReturnType? value,
+        IHttpContextAccessor httpContextAccessor
+
+    )
+    {
+        var errorResponse = Util_GenericResponse<TReturnType>.Response(value, false, ex.ToString(), null, System.Net.HttpStatusCode.InternalServerError);
+
+        _logger.LogError(ex, $"{errorMessage}, [IP] {await Util_GetIpAddres.GetLocation(httpContextAccessor)}", errorResponse);
+
+        errorResponse.Message = "Internal server error";
+
+        return errorResponse;
+    }
+}
---
+++ SafeShare.Utilities/Logs/Util_LogsHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace SafeShare.Utilities.Logs;
+
+public static class Util_LogsHelper<T>
+{
+
+    public static Util_GenericResponse<T>
+    ReturnExeption
+    (
+    )
+    {
+
+    
+    }
+}
