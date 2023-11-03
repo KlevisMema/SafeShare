22599fe0ed9f8f29c3a717fa3bb27820f4832f7a
memak <klevis.mema@ulb.be>
2023-11-03 18:44:20 +0100

    Added blazor client.

---
+++ SafeShare.Client/Shared/Forms/LoginForm.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SafeShare.ClientDTO.Authentication;
+using System.Text.RegularExpressions;
+using System.ComponentModel.DataAnnotations;
+
+namespace SafeShare.Client.Shared.Forms;
+
+public partial class LoginForm
+{
+    [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+
+    void Submit() => MudDialog.Close(DialogResult.Ok(true));
+    void Cancel() => MudDialog.Cancel();
+
+    bool success;
+    string[] errors = { };
+    MudTextField<string> pwField1;
+    public MudForm form;
+
+    public ClientDto_Login clientDto_Login { get; set; } = new ClientDto_Login();
+}
---
+++ SafeShare.ClientDTO/Authentication/ClientDto_Login.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SafeShare.ClientDTO.Authentication;
+
+public class ClientDto_Login
+{
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+}
